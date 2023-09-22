ea3c4caee9e6903faf5b97859c6393d7bc344b57
Cajetan Grill <99743209+krokettenkoal@users.noreply.github.com>
2023-09-22 16:04:35 +0200

    Added basic site description

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,5 @@
-<p>Welcome</p>
+<h1 class="mdc-typography--headline3">UIP showcase</h1>
+<p class="mdc-typography--subtitle1">
+    All code examples covered in the User Interface Programming (UIP) lectures and exercises.<br />
+    UIP is a course in the Media Technology bachelor program at St. Pölten UAS.
+</p>
