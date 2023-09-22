b0619505f34d22a169898cdd3f2f162a7b9ad1e3
Cajetan Grill <99743209+krokettenkoal@users.noreply.github.com>
2023-09-22 09:34:19 +0200

    Added example cards

---
+++ src/lib/showcase/components/ExampleCard.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+    import type {ShowcaseSession, ShowcaseExample} from "$lib/showcase/showcase";
+    import Card, {
+        Content,
+        Media,
+        Actions,
+        ActionButtons,
+    } from '@smui/card';
+    import Button, {Icon, Label} from '@smui/button';
+
+    export let session: ShowcaseSession;
+    export let example: ShowcaseExample;
+</script>
+
+<Card class="example">
+    <Media class="card-media-16x9" aspectRatio="16x9" style="background-image:url(/img/examples/{example.image || 'placeholder.jpg'})" />
+    <Content class="mdc-typography--body2">
+        <h2 class="mdc-typography--headline6" style="margin: 0;">
+            {example.title}
+        </h2>
+        {#if example.subtitle}
+            <h3 class="mdc-typography--subtitle2" style="margin: 0 0 10px; color: #888;">
+                {example.subtitle}
+            </h3>
+        {/if}
+    </Content>
+    <Actions>
+        <ActionButtons>
+            <Button href="/showcase/{session.id}/{example.id}">
+                <Label>View example</Label>
+                <Icon class="material-icons">arrow_forward</Icon>
+            </Button>
+        </ActionButtons>
+    </Actions>
+</Card>
+
+<style>
+    :global(.example) {
+        min-width: 20rem;
+    }
+</style>
--- src/routes/showcase/[session]/+page.svelte
+++ src/routes/showcase/[session]/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     import type {ShowcaseSession} from "$lib/showcase/showcase";
     import Button, {Icon, Label} from "@smui/button";
+    import ExampleCard from "$lib/showcase/components/ExampleCard.svelte";
+    import CardsContainer from "$lib/showcase/components/CardsContainer.svelte";
 
     export let data: ShowcaseSession;
 </script>
@@ -9,5 +11,10 @@
     <Icon class="material-icons">arrow_back_ios</Icon>
     <Label>Back</Label>
 </Button>
+
 <h2>{data.title}</h2>
-{data.examples.map(e => e.title).join(', ')}
+<CardsContainer>
+    {#each data.examples as example}
+        <ExampleCard session={data} {example} />
+    {/each}
+</CardsContainer>
