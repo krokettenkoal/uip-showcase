3f444e94c28dd338cc6a1186d79c10e5a6872e80
Cajetan Grill <99743209+krokettenkoal@users.noreply.github.com>
2023-09-22 09:32:18 +0200

    Added generic card container component

---
+++ src/lib/showcase/components/CardsContainer.svelte
@@ -0,0 +1,11 @@
+<div class="cards">
+    <slot />
+</div>
+
+<style>
+    .cards {
+        display: flex;
+        flex-flow: row wrap;
+        gap: 2rem;
+    }
+</style>
--- src/routes/showcase/+page.svelte
+++ src/routes/showcase/+page.svelte
@@ -1,19 +1,13 @@
 <script lang="ts">
     import type {ShowcaseSession} from "$lib/showcase/showcase";
     import SessionCard from "$lib/showcase/components/SessionCard.svelte";
+    import CardsContainer from "$lib/showcase/components/CardsContainer.svelte";
 
     export let data: {sessions: ShowcaseSession[]};
 </script>
 <h1 class="mdc-typography--headline3">Lecture examples</h1>
-<div class="sessions">
+<CardsContainer>
     {#each data.sessions as session}
         <SessionCard {session} />
     {/each}
-</div>
-
-<style>
-    .sessions {
-        display: flex;
-        flex-flow: row wrap;
-    }
-</style>
+</CardsContainer>
