6a0084f6f1612164d097251bd507a39076f372dd
Cajetan Grill <99743209+krokettenkoal@users.noreply.github.com>
2023-09-22 09:00:36 +0200

    Added session cards

---
+++ src/lib/showcase/components/SessionCard.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+    import type {ShowcaseSession} from "$lib/showcase/showcase";
+    import Card, {
+        Content,
+        Media,
+        Actions,
+        ActionButtons,
+    } from '@smui/card';
+    import Button, {Icon, Label} from '@smui/button';
+
+    export let session: ShowcaseSession;
+</script>
+
+<Card class="session">
+    <Media class="card-media-16x9" aspectRatio="16x9" style="background-image:url({session.image || '/img/sessions/placeholder.jpg'})" />
+    <Content class="mdc-typography--body2">
+        <h2 class="mdc-typography--headline6" style="margin: 0;">
+            {session.title}
+        </h2>
+        {#if session.subtitle}
+            <h3 class="mdc-typography--subtitle2" style="margin: 0 0 10px; color: #888;">
+                {session.subtitle}
+            </h3>
+        {/if}
+        {session.description ?? ''}
+    </Content>
+    <Actions>
+        <ActionButtons>
+            <Button href="/showcase/{session.id}">
+                <Label>View examples</Label>
+                <Icon class="material-icons">arrow_forward</Icon>
+            </Button>
+        </ActionButtons>
+    </Actions>
+</Card>
+
+<style>
+    :global(.session) {
+        min-width: 20rem;
+    }
+</style>
--- src/routes/showcase/+page.svelte
+++ src/routes/showcase/+page.svelte
@@ -1,9 +1,19 @@
 <script lang="ts">
     import type {ShowcaseSession} from "$lib/showcase/showcase";
+    import SessionCard from "$lib/showcase/components/SessionCard.svelte";
 
     export let data: {sessions: ShowcaseSession[]};
 </script>
-<h1>Lecture examples</h1>
-{#each data.sessions as session}
-    {session.title}
-{/each}
+<h1 class="mdc-typography--headline3">Lecture examples</h1>
+<div class="sessions">
+    {#each data.sessions as session}
+        <SessionCard {session} />
+    {/each}
+</div>
+
+<style>
+    .sessions {
+        display: flex;
+        flex-flow: row wrap;
+    }
+</style>
