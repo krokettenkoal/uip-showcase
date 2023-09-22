cba4a84fb753840417ccc20db8864cc83cd74c3e
Cajetan Grill <99743209+krokettenkoal@users.noreply.github.com>
2023-09-22 08:59:44 +0200

    Added back button to session page

--- src/routes/showcase/[session]/+page.svelte
+++ src/routes/showcase/[session]/+page.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
     import type {ShowcaseSession} from "$lib/showcase/showcase";
+    import Button, {Icon, Label} from "@smui/button";
 
     export let data: ShowcaseSession;
 </script>
 
+<Button href="/showcase">
+    <Icon class="material-icons">arrow_back_ios</Icon>
+    <Label>Back</Label>
+</Button>
 <h2>{data.title}</h2>
 {data.examples.map(e => e.title).join(', ')}
