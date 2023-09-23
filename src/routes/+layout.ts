7e63641a37f313a2043e688eb429f77dac2439be
Cajetan Grill <99743209+krokettenkoal@users.noreply.github.com>
2023-09-23 17:36:08 +0200

    Added prerender flag

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,5 +1,6 @@
 import type {LayoutData, LayoutLoad, LayoutLoadEvent} from "./$types";
 import {loadSessions} from "$lib/showcase/showcase";
+export const prerender = true;
 
 export const load: LayoutLoad = async ({}: LayoutLoadEvent): Promise<LayoutData> => {
     // Load all modules found inside showcase components folder
