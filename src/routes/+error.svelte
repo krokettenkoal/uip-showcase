97c4502152a6ef56e484c42dadad59cd7ddb9527
Cajetan Grill <99743209+krokettenkoal@users.noreply.github.com>
2023-09-23 23:12:41 +0200

    Added error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,15 @@
+<script>
+    import {page} from "$app/stores";
+    import {Icon, Label} from "@smui/button";
+    import Hero, {Actions, Button, Title} from "$lib/components/hero";
+</script>
+
+<Hero>
+    <Title>{$page.error.message}</Title>
+    <Actions>
+        <Button href="/">
+            <Icon class="material-icons">arrow_back</Icon>
+            <Label>Back to Home</Label>
+        </Button>
+    </Actions>
+</Hero>
