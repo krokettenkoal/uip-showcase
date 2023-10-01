191b9cab3ebc862ad3cb3345fffa6fbd5e011cc4
Cajetan Grill <99743209+krokettenkoal@users.noreply.github.com>
2023-10-01 21:51:28 +0200

    Changed API factory name

--- src/lib/api/factory.ts
+++ src/lib/api/factory.ts
@@ -2,7 +2,7 @@ import type * as runtime from "$lib/api/runtime";
 import type {FetchAPI} from "$lib/api/runtime";
 import {Configuration} from "$lib/api/runtime";
 
-export function createApi<T extends runtime.BaseAPI>(cls: { new(cfg: Configuration): T ;}, fetchFn: FetchAPI): T {
+export function api<T extends runtime.BaseAPI>(cls: { new(cfg: Configuration): T ;}, fetchFn: FetchAPI): T {
     const cfg = new Configuration({fetchApi: fetchFn});
     return new cls(cfg);
 }
