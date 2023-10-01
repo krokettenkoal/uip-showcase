713682d67312555790ecaafedab36147b5383f2f
Cajetan Grill <99743209+krokettenkoal@users.noreply.github.com>
2023-10-01 21:16:26 +0200

    Updated OpenAPI spec and config - Generating the API client now is part of the prepare routine in package.json - Added local dev and remote API server to OAS - Updated generator options for better control - Added factory method for API client classes, supporting Svelte's fetch implementation - Added a local dev proxy for API routes (primarily for testing)

---
+++ src/lib/api/factory.ts
@@ -0,0 +1,8 @@
+import type * as runtime from "$lib/api/runtime";
+import type {FetchAPI} from "$lib/api/runtime";
+import {Configuration} from "$lib/api/runtime";
+
+export function createApi<T extends runtime.BaseAPI>(cls: { new(cfg: Configuration): T ;}, fetchFn: FetchAPI): T {
+    const cfg = new Configuration({fetchApi: fetchFn});
+    return new cls(cfg);
+}
