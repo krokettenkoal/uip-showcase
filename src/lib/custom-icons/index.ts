454b69af69311f4a78e8bd74cc4ca3715d1e2750
Cajetan Grill <99743209+krokettenkoal@users.noreply.github.com>
2023-10-02 19:47:26 +0200

    Added TypeScript icon

--- src/lib/custom-icons/index.ts
+++ src/lib/custom-icons/index.ts
@@ -1,11 +1,12 @@
 import ciSvelte from "./ciSvelte";
 import ciMoodle from "./ciMoodle";
+import ciTypeScript from "./ciTypeScript";
 import type {IconDefinition, IconName} from "@fortawesome/free-brands-svg-icons";
 
 export type CustomIconName = Exclude<string, IconName>;
 export type CustomIconDefinition = Omit<IconDefinition, 'iconName'> & {iconName: CustomIconName};
 
-export {ciSvelte, ciMoodle};
+export {ciSvelte, ciMoodle, ciTypeScript};
 
 export const loadIcon = async (icon: string|undefined): Promise<IconDefinition|CustomIconDefinition|undefined> => {
     if(!icon)
