55d155b7cf5b1837005bdee7676a419cc47adbe5
Cajetan Grill <99743209+krokettenkoal@users.noreply.github.com>
2023-10-02 19:09:22 +0200

    Added API error handling

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,12 +1,20 @@
 import type {LayoutData, LayoutLoad, LayoutLoadEvent} from "./$types";
 import {CourseApi, SessionApi} from "$lib/api";
 import {api} from "$lib/api/factory";
+import {error} from "@sveltejs/kit";
 export const prerender = true;
 
 export const load: LayoutLoad = async ({fetch}: LayoutLoadEvent): Promise<LayoutData> => {
     const courseApi = api(CourseApi, fetch);
     const sessionApi = api(SessionApi, fetch);
-    const courses = await courseApi.getCourses();
-    const sessions = await Promise.all(courses.map(c => sessionApi.getSessionsByCourse(c.id)));
-    return {courses, sessions};
+    try {
+        const courses = await courseApi.getCourses();
+        const sessions = await Promise.all(courses.map(c => sessionApi.getSessionsByCourse(c.id)));
+        return {courses, sessions};
+    } catch (e: any){
+        throw error(e.response?.status ?? 500, {
+            message: e.message,
+            response: e.response
+        });
+    }
 }
