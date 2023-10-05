ca1775a6abcc0cb6fe9c9d1d44475cebc3fbd6b7
Cajetan Grill <99743209+krokettenkoal@users.noreply.github.com>
2023-10-05 19:02:19 +0200

    Added proxy info

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
 		proxy: {
+			//	Proxy API requests to the local development server (ignored in production)
 			'/api': {
 				target: 'http://localhost:8888',
 				changeOrigin: true,
