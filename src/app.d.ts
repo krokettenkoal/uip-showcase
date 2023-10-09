10e14889ca4b83e64cb1f32a5d690760469395ea
Cajetan Grill <99743209+krokettenkoal@users.noreply.github.com>
2023-10-09 15:30:10 +0200

    Added DB client and server hook The MySQL client uses a connection pool, which is exposed via server hooks (use `event.locals.db` in any server-side request handler). Alternatively, you can directly import the connection pool from `db.ts`. Since the file is saved in `$lib/server`, it should only be accessible by the server, not the client.

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,12 +1,16 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+import type {Pool} from "mysql2/promise";
+
 declare global {
 	namespace App {
 		interface Error {
 			message: string;
 			response?: Response;
 		}
-		// interface Locals {}
+		interface Locals {
+			db: Pool;
+		}
 		// interface PageData {}
 		// interface Platform {}
 	}
