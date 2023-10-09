162d4e162945598d123d60cc6463148fa8a55fee
Cajetan Grill <99743209+krokettenkoal@users.noreply.github.com>
2023-10-09 21:18:00 +0200

    Fixed error handling Unexpected errors are handled by SvelteKit, so no need to include unnecessary custom error handling

--- src/routes/api/source/+server.ts
+++ src/routes/api/source/+server.ts
@@ -1,25 +1,16 @@
 import type {RequestEvent} from "@sveltejs/kit";
-import {json, error, HttpError_1} from '@sveltejs/kit';
+import {json, error} from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
 const sql = 'SELECT Id as id, ExampleId as exampleId, SourceTypeId as typeId, Title as title, Code as code, Priority as priority FROM `sources` WHERE ExampleId = ?';
 
 export const GET: RequestHandler = async ({ locals, url }: RequestEvent) => {
-    try {
-        const exampleId = url.searchParams.get('example');
-        if(!exampleId){
-            throw error(400, {
-                message: `Missing example parameter`
-            });
-        }
-        const [rows] = await locals.db.execute(sql, [exampleId]);
-        return json(rows);
+    const exampleId = url.searchParams.get('example');
+    if(!exampleId){
+        throw error(400, {
+            message: `Missing example parameter`
+        });
     }
-    catch(e){
-        console.error(e);
-        if(HttpError_1 && e instanceof HttpError_1)
-            throw e;
-
-        throw error(500);
-    }
+    const [rows] = await locals.db.execute(sql, [exampleId]);
+    return json(rows);
 };
--- src/routes/api/source/[id]/+server.ts
+++ src/routes/api/source/[id]/+server.ts
@@ -1,24 +1,15 @@
 import type {RequestEvent} from "@sveltejs/kit";
-import {json, error, HttpError_1} from '@sveltejs/kit';
+import {json, error} from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
 const sql = 'SELECT Id as id, ExampleId as exampleId, SourceTypeId as typeId, Title as title, Code as code FROM `sources` WHERE Id = ?';
 
 export const GET: RequestHandler = async ({ params, locals }: RequestEvent) => {
-    try {
-        const [rows] = await locals.db.execute(sql, [params.id]);
-        if(!Array.isArray(rows) || rows.length === 0){
-            throw error(404, {
-                message: `Source code with id ${params.id} not found`
-            });
-        }
-        return json(rows[0]);
+    const [rows] = await locals.db.execute(sql, [params.id]);
+    if(!Array.isArray(rows) || rows.length === 0){
+        throw error(404, {
+            message: `Source code with id ${params.id} not found`
+        });
     }
-    catch(e){
-        console.error(e);
-        if(HttpError_1 && e instanceof HttpError_1)
-            throw e;
-
-        throw error(500);
-    }
+    return json(rows[0]);
 };
