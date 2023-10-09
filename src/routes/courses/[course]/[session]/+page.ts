6fc339ba8d74a8f7a5e8a8f9ff6c40b889cb4a51
Cajetan Grill <99743209+krokettenkoal@users.noreply.github.com>
2023-10-09 18:21:44 +0200

    Changed API/load error handling API errors (`ResponseError`s) are now caught explicitly. This enables us to forward the status code and text to the error page.

--- src/routes/courses/[course]/[session]/+page.ts
+++ src/routes/courses/[course]/[session]/+page.ts
@@ -1,22 +1,29 @@
 import type {PageLoad, PageLoadEvent, PageData} from "./$types";
-import {error} from "@sveltejs/kit";
+import {error, HttpError_1} from "@sveltejs/kit";
 import {api} from "$lib/api/factory";
-import {CourseApi, ExampleApi, SessionApi, SourcetypeApi} from "$lib/api";
+import {CourseApi, ExampleApi, ResponseError, SessionApi, SourcetypeApi} from "$lib/api";
 
 export const load: PageLoad = async ({ params }: PageLoadEvent): Promise<PageData> => {
-    const courseApi = api(CourseApi, fetch);
-    const sessionApi = api(SessionApi, fetch);
-    const exampleApi = api(ExampleApi, fetch);
     const courseId = Number(params.course);
     const sessionId = Number(params.session);
     if(!courseId || !sessionId)
-        throw error(404);
+        throw error(400, `Invalid course id ${courseId} and/or session id ${sessionId}`);
 
-    const course = await courseApi.getCourseById(courseId);
-    const session = await sessionApi.getSessionById(sessionId);
-    if(!course || !session)
-        throw error(404);
+    const courseApi = api(CourseApi, fetch);
+    const sessionApi = api(SessionApi, fetch);
+    const exampleApi = api(ExampleApi, fetch);
 
-    const examples = exampleApi.getExamplesBySession(sessionId);
-    return {course, session, examples};
+    try {
+        const course = await courseApi.getCourseById(courseId);
+        const session = await sessionApi.getSessionById(sessionId);
+        const examples = exampleApi.getExamplesBySession(sessionId);
+        return {course, session, examples};
+    }
+    catch(e){
+        console.error(e);
+        if(e instanceof ResponseError)
+            throw error(e.response.status, e.response.statusText);
+
+        throw error(500);
+    }
 }
