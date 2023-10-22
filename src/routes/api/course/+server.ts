40e64825de0eeac26aef3744a270422105ac0fb0
Cajetan Grill <99743209+krokettenkoal@users.noreply.github.com>
2023-10-22 10:11:16 +0200

    Added API POST routes This should have been committed alongside the latest changes on the `showcase-api.json` file

--- src/routes/api/course/+server.ts
+++ src/routes/api/course/+server.ts
@@ -1,16 +1,24 @@
 import type {RequestEvent} from "@sveltejs/kit";
 import {json, error} from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
+import type {ResultSetHeader} from "mysql2";
+import type {Course} from "$lib/api";
 
-const sql = 'SELECT Id as id, StudyProgramId as studyProgramId, Title as title, Subtitle as subtitle, MoodleUrl as moodleUrl FROM `courses`';
+const querySql = 'SELECT Id as id, StudyProgramId as studyProgramId, Title as title, Subtitle as subtitle, MoodleUrl as moodleUrl FROM `courses`';
+const insertSql = 'INSERT INTO `courses` (StudyProgramId, Title, Subtitle, MoodleUrl) VALUES (?, ?, ?, ?)';
 
 export const GET: RequestHandler = async ({ locals }: RequestEvent) => {
-    try {
-        const [rows] = await locals.db.query(sql);
-        return json(rows);
-    }
-    catch(e){
-        console.error(e);
-        throw error(500);
-    }
+    const [rows] = await locals.db.query(querySql);
+    return json(rows);
 };
+
+export const POST: RequestHandler = async ({ params, locals, request }: RequestEvent) => {
+    const data = await request.json();
+    if(!data.studyProgramId || !data.title){
+        const missing = [data.studyProgramId && 'studyProgramId', data.title && 'title'].filter(Boolean) as string[];
+        throw error(400, 'Missing required field(s): ' + missing.join(', '));
+    }
+    const [result] = await locals.db.execute<ResultSetHeader>(insertSql, [data.studyProgramId, data.title, data.subtitle ?? null, data.moodleUrl ?? null]);
+    const course = {id: result.insertId, ...data} satisfies Course;
+    return json(course);
+}
--- src/routes/api/studyprogram/+server.ts
+++ src/routes/api/studyprogram/+server.ts
@@ -1,16 +1,23 @@
 import type {RequestEvent} from "@sveltejs/kit";
-import {json, error} from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
+import type {ResultSetHeader} from "mysql2";
+import type { StudyProgram} from "$lib/api";
+import {json, error} from '@sveltejs/kit';
 
 const sql = 'SELECT Id as id, Title as title, Subtitle as subtitle FROM `studyprograms`';
+const insertSql = 'INSERT INTO `studyprograms` (Title, Subtitle) VALUES (?, ?)';
 
 export const GET: RequestHandler = async ({ locals }: RequestEvent) => {
-    try {
-        const [rows] = await locals.db.query(sql);
-        return json(rows);
-    }
-    catch(e){
-        console.error(e);
-        throw error(500);
-    }
+    const [rows] = await locals.db.query(sql);
+    return json(rows);
 };
+
+export const POST: RequestHandler = async ({ params, locals, request }: RequestEvent) => {
+    const data = await request.json();
+    if(!data.title){
+        throw error(400, 'Missing required field: title');
+    }
+    const [result] = await locals.db.execute<ResultSetHeader>(insertSql, [data.title, data.subtitle ?? null]);
+    const studyProgram = {id: result.insertId, ...data} satisfies StudyProgram;
+    return json(studyProgram);
+}
