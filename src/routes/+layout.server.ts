60568eabb0ab952ad139501037673722fc6b7836
Cajetan Grill <99743209+krokettenkoal@users.noreply.github.com>
2023-10-22 10:12:06 +0200

    Added repo name and owner to base layout load These changes made the page loader for the admin dashboard obsolete (at least for now)

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,7 +1,8 @@
 import type { LayoutServerLoad, LayoutServerLoadEvent, LayoutServerData } from "./$types"
+import {error} from "@sveltejs/kit";
 import {api, studyProgramCache} from "$lib/api/factory";
 import {CourseApi, ResponseError, SessionApi, StudyprogramApi} from "$lib/api";
-import {error} from "@sveltejs/kit";
+import {PUBLIC_REPO_OWNER, PUBLIC_REPO_NAME} from "$env/static/public";
 
 export const load: LayoutServerLoad = async ({locals, fetch}: LayoutServerLoadEvent): Promise<LayoutServerData> => {
     const studyProgramApi = api(StudyprogramApi, fetch);
@@ -13,7 +14,14 @@ export const load: LayoutServerLoad = async ({locals, fetch}: LayoutServerLoadEv
         studyProgramCache.add(...studyPrograms);
         const courses = await courseApi.getCourses();
         const sessions = await Promise.all(courses.map(c => sessionApi.getSessionsByCourse(c.id)));
-        return {authSession, studyPrograms, courses, sessions};
+        return {
+            authSession,
+            studyPrograms,
+            courses,
+            sessions,
+            repoOwner: PUBLIC_REPO_OWNER,
+            repoName: PUBLIC_REPO_NAME
+        };
     } catch (e){
         console.error(e);
         if(e instanceof ResponseError)
--- src/routes/admin/+page.ts
+++
@@ -1,7 +0,0 @@
-import {PUBLIC_REPO_OWNER, PUBLIC_REPO_NAME} from "$env/static/public";
-import type {PageLoad, PageLoadEvent, PageData} from "./$types";
-
-export const load: PageLoad = async ({fetch, params, parent}: PageLoadEvent): Promise<PageData> => {
-    const {authSession} = await parent();
-    return {authSession, repoOwner: PUBLIC_REPO_OWNER, repoName: PUBLIC_REPO_NAME};
-}
